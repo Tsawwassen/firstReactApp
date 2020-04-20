21b561e211c7aa76e59e121c8d65a2c97f3e93dd
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-04-20 11:50:11 -0700

    Add navbar componenet to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers/customers'
+import Customers from './components/customers/customers';
+import  './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Navbar />
       <Customers />
     </div>
   );
