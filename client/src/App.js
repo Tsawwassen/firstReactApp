59c82ed782349f102b8cb0136ebc65fa1a68e82d
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-04-20 12:09:00 -0700

    Add function to view different pages on the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Customers from './components/customers/customers';
 import  './App.css';
 import Navbar from './components/navbar/navbar';
+import Employee from './components/employee/employee';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Customers />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/employee">
+            <Employee />
+          </Route>
+          <Route path="/customer">
+            <Customers />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+
   );
 }
 
