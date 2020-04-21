d11b32993d4667fc2a1229d842e0f63134efc286
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-04-21 10:44:51 -0700

    Update navbar compnenet to AppNavBar

---
+++ client/src/components/appnavbar/appnavbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './appnavbar.css';
+import {Navbar, Nav} from 'react-bootstrap';
+
+
+
+class AppNavbar extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+
+  }
+
+  render () {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">ReactApp</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/customer">Customer</Nav.Link>
+             <Nav.Link href="/employee">Employee</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
