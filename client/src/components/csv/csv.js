88e454e0351ec21ae0f5bb2350165d516f370a54
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-04-21 20:15:41 -0700

    Clean up CSV component code

--- client/src/components/csv/csv.js
+++ client/src/components/csv/csv.js
@@ -1,40 +1,68 @@
 import React, { Component } from 'react';
 import CSVReader from 'react-csv-reader'
 
-
+/**************************************88
+*
+* CSV Component is used to upload a file, parse the file, and upload the file data to the backend server
+*
+****************************************/
 class CSV extends Component {
-
-
 
   constructor(props){
     super(props);
     this.state = {
     	filePathValue: '',
-    	papaparseOptions: {
-	    	header: true,
-	    	dynamicTyping: true,
-	    	skipEmptyLines: true,
-	    	transformHeader: header =>
-	      	header
-	        	.toLowerCase()
-	        	.replace(/\W/g, '_')
-	  		}	
-
     }
 
+    //Expected headers that should be in the CSV file
+		this.EXPECTED_HEADER = [ "test", "name"].sort();
 
+		//Headers received from CSV file
+		this.receivedHeader = [];
+
+		//CSV Parser options
+		this.papaparseOptions = {
+	    header: true, 
+	    dynamicTyping: true,
+	    skipEmptyLines: true,
+	    transformHeader: (header) => {
+	    	//This callback function is called once for each header in the CSV file (header: true)
+	     	header.toLowerCase().replace(/\W/g, '_');
+
+      	//Push headers to receivedHeader array
+        this.receivedHeader.push(header);
+    	}
+  	}
+
+  	/** Component Function binds **/
   	this.handleFileUpload = this.handleFileUpload.bind(this);
   	this.handleFileError = this.handleFileUpload.bind(this);
-
-
   }
 
+  //Handle file data
 	handleFileUpload(csvData, fileInfo){
-		console.log(csvData);
+
+		// Note about keys
+		//-------------------------
+		//Gets keys for a given JSON object
+		//console.log(Object.keys(csvData[0]));
+		//console.log(csvData);
+		//console.log(fileInfo);
+
+		// Note about checking headers
+		//-------------------------
+		//The below logic will test if the received header is the same as the expected header.
+		//The expected header is sorted when it is initialized, and the received header is checked before the two are compared
+		//I'm not sure how I feel about converting them to a string, then comparing them, but it works
+		//
+		//This also asumes that the CSV files does not have any other headers.
+		//Better option would to see if the received headers included expected headers 
+		//console.log( JSON.stringify(this.EXPECTED_HEADER) === JSON.stringify(this.receivedHeader.sort()) );
 
 		//Send list of names to backend server
 		//On the server, for each name sent, create the next id, and add it to the database
 
+		//Fetch options
 		const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -42,18 +70,16 @@ class CSV extends Component {
     };
     
     fetch('/api/customers/batch', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-        	console.log(data);
-        });
-
+      .then(response => response.json())
+      .then(data => {
+       	console.log(data);
+    	}
+    );
   }
-
 
 	handleFileError(event){
 		console.log("error");
 	}
-
 
   render () {
     return (
@@ -64,7 +90,7 @@ class CSV extends Component {
 			        label="Select CSV"
 			        onFileLoaded={this.handleFileUpload}
 			        onError={this.handleFileError}
-			        parserOptions={this.state.papaparseOptions}
+			        parserOptions={this.papaparseOptions}
 			        inputId="csv"
      				/>
       </div>
